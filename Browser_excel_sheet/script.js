cfb45c2c59671bf68709be3ea72fa02d4d4438a5
shaik samiulla <sksamiulla1111@gmail.com>
2021-05-05 17:40:42 +0530

    first commit

---
+++ Browser_excel_sheet/script.js
@@ -0,0 +1,103 @@
+console.log("hello");
+let addbtnContainer = document.querySelector(".add-sheet_container");
+let sheetList = document.querySelector(".sheets-list");
+let firstSheet = document.querySelector(".sheet");
+let Allcells = document.querySelectorAll(".grid .col");
+let leftBtn = document.querySelector(".left");
+let rightBtn = document.querySelector(".right");
+let centerBtn = document.querySelector(".center");
+let fontBtn = document.querySelector(".font-size");
+let addressBar = document.querySelector(".address-box");
+
+
+firstSheet.addEventListener("click",function(){
+    let sheetsArr = document.querySelectorAll(".sheet");
+    sheetsArr.forEach(function(sheet){
+        sheet.classList.remove("active-sheet");
+    })
+    if(!firstSheet.classList[1]){
+        NewSheet.classList.add("active-sheet");
+    }
+})
+
+
+
+addbtnContainer.addEventListener("click",function(){
+    let sheetsArr = document.querySelectorAll(".sheet");
+    let lastSheetElem = sheetsArr[sheetsArr.length - 1];
+    let idx = lastSheetElem.getAttribute("sheetIdx");
+    idx=Number(idx);
+
+    let NewSheet = document.createElement("div");
+    NewSheet.setAttribute("class","sheet");
+    NewSheet.setAttribute("sheetIdx",idx+1);
+    NewSheet.innerText =`Sheet ${idx + 2}`;
+
+    sheetList.appendChild(NewSheet);
+    NewSheet.addEventListener("click",function(){
+        let sheetsArr = document.querySelectorAll(".sheet");
+        sheetsArr.forEach(function(sheet){
+            sheet.classList.remove("active-sheet");
+        })
+        if(!NewSheet.classList[1]){
+            NewSheet.classList.add("active-sheet");
+        }
+    })
+
+})
+
+for(let i=0;i<Allcells.length;i++){
+    Allcells[i].addEventListener("click",function handleCell(){
+        let rid = Number(Allcells[i].getAttribute("rid"));
+        let cid = Number(Allcells[i].getAttribute("cid"));
+        let rowAdd = rid + 1;
+        let colAdd = String.fromCharCode(cid+65);
+        let address = colAdd + rowAdd;
+        addressBar.value = address;
+    })
+}
+leftBtn.addEventListener("click", function () {
+    let address = addressBar.value;
+    let { rid, cid } = getRIdCIdfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign = "left"
+})
+rightBtn.addEventListener("click", function () {
+    let address = addressBar.value;
+    let { rid, cid } = getRIdCIdfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign = "right"
+})
+centerBtn.addEventListener("click", function () {
+    let address = addressBar.value;
+    let { rid, cid } = getRIdCIdfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign = "center"
+})
+
+fontBtn.addEventListener("change", function () {
+    let fontSize = fontBtn.value;
+    let address = addressBar.value;
+    let { rid, cid } = getRIdCIdfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    console.log(fontSize);
+    cell.style.fontSize = fontSize+"px";
+})
+function getRIdCIdfromAddress(adress) {
+    // A1
+    let cellColAdr = adress.charCodeAt(0);
+    // console.log(cellColAdr);
+    let cellrowAdr = adress.slice(1);
+    let cid = cellColAdr - 65;
+    let rid = Number(cellrowAdr) - 1;
+    return { cid, rid };
+
+}
+
+
+
+Allcells[0].click();
